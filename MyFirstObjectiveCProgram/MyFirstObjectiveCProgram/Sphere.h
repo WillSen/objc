3c08a36ffab97680c81b97d15790dd702a8f162a
Will Sentance <william.sentance@gmail.com>
2015-01-09 18:04:43 -0800

    initial commit

---
+++ MyFirstObjectiveCProgram/MyFirstObjectiveCProgram/Sphere.h
@@ -0,0 +1,15 @@
+//
+//  Sphere.h
+//  MyFirstObjectiveCProgram
+//
+//  Created by Will Sentance on 1/9/15.
+//  Copyright (c) 2015 treehouse. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Sphere : NSObject {
+    NSArray *center
+}
+
+@end
---
+++ MyFirstObjectiveCProgram/MyFirstObjectiveCProgram/Sphere.m
@@ -0,0 +1,13 @@
+//
+//  Sphere.m
+//  MyFirstObjectiveCProgram
+//
+//  Created by Will Sentance on 1/9/15.
+//  Copyright (c) 2015 treehouse. All rights reserved.
+//
+
+#import "Sphere.h"
+
+@implementation Sphere
+
+@end
